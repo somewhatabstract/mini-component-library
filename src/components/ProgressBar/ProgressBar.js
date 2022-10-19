8a041d960b57fe324f4b9265466a5c2018183451
Jeff Yates <jeff@khanacademy.org>
2022-10-19 13:21:08 -0500

    Implement ProgressBar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,64 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const wrapperStyles = {
+  small: {
+    '--border-radius': '4px',
+    '--padding': '0px',
+    '--height': '8px',
+  },
+  medium: {
+    '--border-radius': '4px',
+    '--padding': '0px',
+    '--height': '12px',
+  },
+  large: {
+    '--border-radius': '8px',
+    '--padding': '4px',
+    '--height': '24px',
+  },
+};
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--border-radius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  height: var(--height);
+`;
+
+const InnerBorderClipper = styled.div`
+  border-radius: 4px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: var(--width);
+  border-radius: 4px 0 0 4px;
+  height: 100%;
+  overflow: hidden;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const normalizedValue = Math.min(Math.max(0, value), 100);
+  const width = `${normalizedValue}%`;
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={wrapperStyles[size]}
+    >
+      <InnerBorderClipper>
+        <Bar style={{"--width": width}} />
+        <VisuallyHidden>{width}</VisuallyHidden>
+      </InnerBorderClipper>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
