d68025ac2dc9da30cbcb5eceb4ccd2c79364cadd
Jeff Yates <jeff@khanacademy.org>
2022-10-19 14:43:04 -0500

    Implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,15 +3,61 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
+import VisuallyHidden from '../VisuallyHidden/VisuallyHidden.js';
 import { getDisplayedValue } from './Select.helpers';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const HiddenNativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const CustomSelect = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: 1rem;
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+
+  :focus-within {
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  :hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: calc(50% - 12px);
+  right: 12px;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <CustomSelect>
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} strokeWidth={2} />
+        </IconWrapper>
+        <HiddenNativeSelect value={value} onChange={onChange}>
+        {children}
+        </HiddenNativeSelect>
+        {displayedValue}
+      </CustomSelect>
+    </Wrapper>
   );
 };
 
